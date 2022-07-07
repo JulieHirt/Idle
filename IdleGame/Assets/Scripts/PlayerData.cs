7d05dc84391472a2fbfd790bdc4d6c94292824c9
JulieBodette <jr3python@gmail.com>
2022-07-07 12:51:15 -0400

    Player Data created
    cash is loaded via PlayerPrefs on game start and is displayed to the screen

---
+++ IdleGame/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerData : MonoBehaviour
+{
+    //Singleton
+    public static PlayerData data; //static reference
+
+    public int START_CASH = 1000; //starting cash for the player on initial game start
+    private int cash;
+    private int numGenerators;
+
+    public Text cashText;
+
+    public int getCash()
+    {
+        return cash;
+    }
+    public void setCash(int num)
+    {
+        cash = num;
+    }
+    public int getNumGenerators()
+    {
+        return numGenerators;
+    }
+    public void setNumGenerators(int num)
+    {
+        numGenerators = num;
+    }
+    void Start()
+    {
+        //call on game start
+
+        //will load the cash and set it to START_CASH if there is no saved value
+        cash = PlayerPrefs.GetInt("cash", START_CASH);
+        Debug.Log(cash);
+    }
+
+    private void Update()
+    {
+        cashText.text = "Cash: " + cash;
+    }
+
+
+    void Awake()
+
+    {
+        if (data == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            data = this;
+        }
+        else if (data != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
+
